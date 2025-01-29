abcd60bb41e0a0e1465596f0a3edcd6a2bcd6c0f
incisrdg <128684506+incisrdg@users.noreply.github.com>
2025-01-29 10:51:27 +0300

    Create olf_pca_traj.R

---
+++ scripts/olf_pca_traj.R
@@ -0,0 +1,19 @@
+library(bio3d)
+
+mypdbfile <- "/path_to_pdb"
+mydcdfile <- "/path_to_dcd"
+
+dcd <- read.dcd(mydcdfile)
+pdb <- read.pdb(mypdbfile)
+
+ca.inds <- atom.select(pdb, elety="CA")
+
+xyz <- fit.xyz(fixed=pdb$xyz, mobile=dcd,
+               fixed.inds=ca.inds$xyz,
+               mobile.inds=ca.inds$xyz)
+
+pc <- pca.xyz(xyz[,ca.inds$xyz])
+
+hc <- hclust(dist(pc$z[,1:2]))
+grps <- cutree(hc, k=2)
+plot(pc, col=grps)
